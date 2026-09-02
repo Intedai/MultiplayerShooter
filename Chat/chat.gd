eb7f5cb07b907c82f6f5dd97776ef6d5536aceed
Intedai <arielsync@proton.me>
2026-09-02 03:26:00 +0300

    Add scrollable chat text, with usernames (pid for now) and join messages, move the chat to top left and move the buttons that were there

--- Chat/chat.gd
+++ Chat/chat.gd
@@ -1,19 +1,49 @@
 extends Control
 
+@onready var chat_text: Label = $MarginContainer/HBoxContainer/VBoxContainer/ScrollContainer/ChatText
 @onready var line_edit: LineEdit = $MarginContainer/HBoxContainer/VBoxContainer/LineEdit
+@onready var scroll_container: ScrollContainer = $MarginContainer/HBoxContainer/VBoxContainer/ScrollContainer
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("chat") and !line_edit.has_focus():
-		visible = not visible
+		_toggle_input()
+		scroll_to_bottom()
 		await get_tree().process_frame
 		line_edit.grab_focus()
 		
 		return
 	
-	if event.is_action_pressed("send_chat_msg") and line_edit.has_focus():
+	if event.is_action_pressed("ui_text_submit") and line_edit.has_focus():
 		line_edit.release_focus()
-		visible = not visible
+		_toggle_input()
 		
-		# TODO: Add send message logic
-		print(str(multiplayer.get_unique_id()) + ": " + line_edit.text)
+		var msg = format_chat_msg(line_edit.text)
+		
+		if len(msg) != 0:
+			add_msg.rpc(msg, true)
+		
 		line_edit.clear()
+
+	elif event.is_action_pressed("ui_cancel") and line_edit.has_focus():
+		line_edit.release_focus()
+		_toggle_input()
+		line_edit.clear()
+
+@rpc("any_peer", "call_local", "reliable")
+func add_msg(msg: String, sent_by_player: bool) -> void:
+	if sent_by_player:
+		msg = msg.insert(0, "<" + str(multiplayer.get_remote_sender_id()) + "> ")
+	chat_text.text += "\n" + msg
+	scroll_to_bottom()
+	
+func format_chat_msg(msg: String) -> String:
+	# Strip whitespaces and turn multiple spaces into one
+	return " ".join(msg.strip_edges().split(" ", false))
+
+func _toggle_input() -> void:
+	line_edit.visible = not line_edit.visible
+
+func scroll_to_bottom() -> void:
+	for i in range(2):
+		await get_tree().process_frame
+	scroll_container.scroll_vertical = int(scroll_container.get_v_scroll_bar().max_value)
