c68aca11cd003bd0c019f5d1238f70588be4ff89
Intedai <arielsync@proton.me>
2026-09-15 21:36:36 +0300

    Menu works now

--- MainScenes/Game/game.gd
+++ MainScenes/Game/game.gd
@@ -4,37 +4,32 @@ const PLAYER = preload("res://Player/player.tscn")
 
 @onready var player_spawner: MultiplayerSpawner = $PlayerSpawner
 @onready var spawn_point: Marker2D = $SpawnPoint
-@onready var multiplayer_ui: Control = $UI/MarginContainer/Multiplayer
 @onready var total_score_label: Label = $UI/MarginContainer/TotalScore
 @onready var chat: Chat = $UI/MarginContainer/Chat
-
-var peer = ENetMultiplayerPeer.new()
 
 var total_score = 0
 
 func _ready() -> void:
 	player_spawner.spawn_function = spawn_player
+	
+	if multiplayer.is_server():
+		player_spawner.spawn(1)
+
+		multiplayer.peer_connected.connect(_on_peer_connected)
+		multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+
+func _on_peer_connected(pid: int) -> void:
+	chat.add_msg.rpc(str(pid) + " joined the game!", false)
+	player_spawner.spawn(pid)
+
+func _on_peer_disconnected(pid: int) -> void:
+	chat.add_msg.rpc(str(pid) + " left the game!", false)
+	# TODO: Remove player
 
 @rpc("call_local", "reliable")
 func update_total_score(new_value: int) -> void:
 	total_score += new_value
 	total_score_label.text = "Total Score: " + str(total_score)
-
-func _on_host_pressed() -> void:
-	peer.create_server(10101)
-	multiplayer.multiplayer_peer = peer
-	multiplayer.peer_connected.connect(
-		func(pid):
-			chat.add_msg.rpc(str(pid) + " joined the game!", false)
-			player_spawner.spawn(pid)
-	)
-	multiplayer_ui.hide()
-	player_spawner.spawn(1)
-
-func _on_join_pressed() -> void:
-	peer.create_client("localhost", 10101)
-	multiplayer.multiplayer_peer = peer
-	multiplayer_ui.hide()
 
 func spawn_player(pid) -> Player:
 	var player: Player = PLAYER.instantiate()
--- MainScenes/MainMenu/main_menu.gd
+++ MainScenes/MainMenu/main_menu.gd
@@ -1,18 +1,30 @@
 extends Control
 
 @onready var main_menu_buttons: VBoxContainer = $MainMenuButtons
+@onready var host_server_menu: VBoxContainer = $HostServerMenu
 @onready var join_server_menu: VBoxContainer = $JoinServerMenu
 @onready var connection_screen: VBoxContainer = $ConnectionScreen
 
+@onready var host_port_input: LineEdit = $HostServerMenu/HBoxContainer/PortInput
+
 # Addr
 @onready var address_input: LineEdit = $JoinServerMenu/AddrContrainer/AddressInput
-@onready var port_input: LineEdit = $JoinServerMenu/AddrContrainer/PortInput
+@onready var join_port_input: LineEdit = $JoinServerMenu/AddrContrainer/PortInput
 
-var peer = ENetMultiplayerPeer.new()
+func _ready() -> void:
+	# Client failed to connect to server
+	multiplayer.connection_failed.connect(func(): print("failed to connect"))
+	
+	# Client connected successfully to server
+	multiplayer.connected_to_server.connect(
+		func():
+			get_tree().change_scene_to_file("res://MainScenes/Game/game.tscn")
+	)
 
 # Main menu btns
 func _on_host_pressed() -> void:
-	print("host pressed")
+	main_menu_buttons.visible = false
+	host_server_menu.visible = true
 
 func _on_join_pressed() -> void:
 	main_menu_buttons.visible = false
@@ -21,18 +33,53 @@ func _on_join_pressed() -> void:
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
+# Host server menu
+func _on_host_cancel_pressed() -> void:
+	host_port_input.clear()
+	host_server_menu.visible = false
+	main_menu_buttons.visible = true
+
+func _on_start_pressed() -> void:
+	print("Starting server in port: " + str(int(host_port_input.text)))
+	
+	var peer = ENetMultiplayerPeer.new()
+	var err = peer.create_server(int(host_port_input.text))
+	
+	if err:
+		print(err)
+		return
+
+	multiplayer.multiplayer_peer = peer
+	
+	host_port_input.clear()
+	
+	get_tree().change_scene_to_file("res://MainScenes/Game/game.tscn")
+
 # Join server menu
 func clear_inputs():
 	address_input.clear()
-	port_input.clear()
+	join_port_input.clear()
 
-func _on_cancel_pressed() -> void:
+func _on_join_cancel_pressed() -> void:
 	clear_inputs()
 	join_server_menu.visible = false
 	main_menu_buttons.visible = true
 
 func _on_connect_pressed() -> void:
-	print("connecting to " + address_input.text + ":" + port_input.text)
-	clear_inputs()
+	print("connecting to " + address_input.text + ":" + join_port_input.text)
+
 	join_server_menu.visible = false
 	connection_screen.visible = true
+	
+	var peer = ENetMultiplayerPeer.new()
+
+	print("port:" + str(int(join_port_input.text)))
+	var err = peer.create_client(address_input.text, int(join_port_input.text))
+
+	if err:
+		print(err)
+		return
+
+	multiplayer.multiplayer_peer = peer
+
+	clear_inputs()
